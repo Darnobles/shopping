df4de61addbd22cac6014eef042bf917e8e8fe41
Darnobles <126102632+Darnobles@users.noreply.github.com>
2024-01-16 15:05:55 -0500

    add shoppingList and Welcome file

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import ShoppingLists from "./components/ShoppingLists";
 
-export default function App() {
+// Create the navigator
+const Stack = createNativeStackNavigator();
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { LogBox } from "react-native";
+LogBox.ignoreLogs(["AsyncStorage has been extracted from"]);
+
+const App = () => {
+  const firebaseConfig = {
+    apiKey: "AIzaSyCWQPiKXNrGVFNHMXdel-mbcTkCbWwRoH8",
+    authDomain: "shopping-list-demo-6ff71.firebaseapp.com",
+    projectId: "shopping-list-demo-6ff71",
+    storageBucket: "shopping-list-demo-6ff71.appspot.com",
+    messagingSenderId: "975573028880",
+    appId: "1:975573028880:web:869c131ea033d4fbd47a95",
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // Initialize Cloud Firestore and get a reference to the service
+  const db = getFirestore(app);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="ShoppingLists">
+        <Stack.Screen name="Welcome" component={Welcome} />
+        <Stack.Screen name="ShoppingLists">
+          {(props) => <ShoppingLists db={db} {...props} />}
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
